37d6e2c5c7e844077e26cc09a930cb5ec965e746
Arthur Santos <arthur.silva1@hotmail.com.br>
2019-10-12 18:24:20 -0300

    Implement routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 
-// import { Container } from './styles';
+import SingIn from "../pages/SingIn";
+import SingUp from "../pages/SingUp";
 
-export default function routes() {
-  return <div />;
+import Dashboard from "../pages/Dashboard";
+import Profile from "../pages/Profile";
+
+export default function Routes() {
+  return (
+    <Switch>
+      <Route path="/" exact component={SingIn} />
+      <Route path="/register" component={SingUp} />
+
+      <Route path="/dashboard" component={Dashboard} />
+      <Route path="/profile" component={Profile} />
+    </Switch>
+  );
 }
