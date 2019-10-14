0b952bcadc5b79d5e604e3d2c75d4964dd7774ce
Arthur Santos <arthur.silva1@hotmail.com.br>
2019-10-14 00:33:45 -0300

    Implement rocketset/unform lib

--- src/pages/SingIn/index.js
+++ src/pages/SingIn/index.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Form, Input } from "@rocketseat/unform";
 
-// import { Container } from './styles';
+import logo from "~/assets/logo.svg";
 
 export default function SingIn() {
-  return <h1>teste</h1>;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register"> Criar conta gratuita </Link>
+      </Form>
+    </>
+  );
 }
