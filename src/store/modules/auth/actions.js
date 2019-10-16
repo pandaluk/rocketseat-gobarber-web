578644b3208457b5d17b6d547169909e1afcc6aa
Arthur Santos <arthur.silva1@hotmail.com.br>
2019-10-16 19:13:10 -0300

    Implements singin failure

--- src/store/modules/auth/actions.js
+++ src/store/modules/auth/actions.js
@@ -12,8 +12,8 @@ export function signInSuccess(token, user) {
   };
 }
 
-export function singFailure() {
+export function signFailure() {
   return {
-    type: "@auth/SIGN_FAILURE"
+    type: "@auth/SIGN_IN_FAILURE"
   };
 }
